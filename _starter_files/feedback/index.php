fbb40ab89020894cc53cb9a8726443202b86023a
Kalle <kalletiihonen@hotmail.com>
2023-02-14 13:12:25 +0100

    finished tutorial

--- _starter_files/feedback/index.php
+++ _starter_files/feedback/index.php
@@ -1,6 +1,5 @@
 <?php include 'inc/header.php'; ?>
 
-
 <?php 
 $name = $email = $body = '';
 $nameErr = $emailErr = $bodyErr = '';
@@ -13,24 +12,60 @@ if(isset($_POST['submit'])) {
   } else {
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   }
+
+  // validate email
+  if(empty($_POST['email'])) {
+    $emailErr = 'Email is required';
+  } else {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+
+  // validate name
+  if(empty($_POST['body'])) {
+    $bodyErr = 'Feedback is required';
+  } else {
+    $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+  if(empty($nameErr) && empty($emailErr) && empty($bodyErr)){
+    // Add to database
+    $sql = "INSERT INTO FEEDBACK (name, email, body) VALUES ('$name', '$email', '$body')";
+    
+    if(mysqli_query($conn, $sql)) {
+      // Success
+      header('Location: feedback.php');
+    } else {
+      // Error
+      echo 'Error: ' . mysqli_error($conn);
+    }
+  }
 }
+
 ?>
+
 <img src="/php-crash-course/_starter_files/feedback/img/logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
     <p class="lead text-center">Leave feedback for Traversy Media</p>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo $nameErr ? 'is-invalid' : null; ?>" id="name" name="name" placeholder="Enter your name">
+     <div class="invalid-feedback">
+      <?php echo $nameErr;  ?>
+     </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        <input type="email" class="form-control <?php echo $emailErr ? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email">
       </div>
+      <?php echo $emailErr;  ?>
       <div class="mb-3">
         <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <textarea class="form-control <?php echo $bodyErr ? 'is-invalid' : null; ?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
       </div>
+      <div class="invalid-feedback">
+      <?php echo $bodyErr;  ?>
+     </div>
       <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
       </div>
